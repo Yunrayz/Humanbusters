66b5d23dbeef21c95c5e65f012e9796404befd4c
Isabel <isadede@alum.us.es>
2021-12-03 18:05:32 +0100

    kitchenNPCs

--- Assets/Scripts/Woman1.cs
+++ Assets/Scripts/Woman1.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Man1 : MonoBehaviour
+public class Woman1 : MonoBehaviour
 {
     public float speed;
     public int fear;                //fear the NPC is feeling
     public int fearLimit = 100;     //limit of Fear before he runs away
     public float radius = 2;        //min distance between the NPC and the action for him to be affected
     private bool running;
-
+    private int stop;
 
     //get from interaction scripts
     public bool actionTriggered = false;
@@ -17,7 +17,7 @@ public class Man1 : MonoBehaviour
     public float yPosAction = 5;
 
     private Rigidbody2D neighborRb;
-    private Vector2 direction = Vector2.down;
+    private Vector2 direction = Vector2.left;
     private NPCFunctions functionsScript;       //script with functions common to all NPCs
     private AudioSource audioSource;
 
@@ -30,6 +30,7 @@ public class Man1 : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
         running = false;
         speed = 1;
+        stop = 0;
     }
 
     // Update is called once per frame
@@ -49,8 +50,10 @@ public class Man1 : MonoBehaviour
             if (!running)
                 audioSource.Play();
             RunAway();
-        } else { 
-            Move(); 
+        }
+        else
+        {
+            Move();
         }
 
     }
@@ -58,14 +61,14 @@ public class Man1 : MonoBehaviour
     private void Move()
     {
 
-        if (transform.position.y >= 0)
+        if (transform.position.x <= 3)
         {
-            direction = Vector2.down;
+            direction = Vector2.right;
             neighborRb.velocity = speed * direction;
         }
-        else if (transform.position.y <= -2.5)
+        else if (transform.position.x >= 4.5)
         {
-            direction = Vector2.up;
+            direction = Vector2.left;
             neighborRb.velocity = speed * direction;
         }
         else
@@ -79,13 +82,25 @@ public class Man1 : MonoBehaviour
     {
         running = true;
         speed = 2.5f;
-        if (transform.position.y < 1)
-            neighborRb.velocity = speed * Vector2.up;
-        else if (transform.position.x < 5.5)
-            neighborRb.velocity = speed * Vector2.right;
-        else if (transform.position.y < 1.8)
-            neighborRb.velocity = speed * Vector2.up;
-        else
-            Destroy(this.gameObject);
+        
+
+        switch (stop)
+        {
+            case 0:
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(1.5f, -3), speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, new Vector2(1.5f, -3)) < 0.001f)
+                    stop = 1;
+                break;
+            case 1:
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(-5.5f, -3), speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, new Vector2(-5.5f, -3)) < 0.001f)
+                    stop = 2;
+                break;
+            case 2:
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(-5.5f, 1), speed * Time.deltaTime);
+                if (Vector2.Distance(transform.position, new Vector2(-5.5f, 1)) < 0.001f)
+                    Destroy(this.gameObject);
+                break;
+        }
     }
 }
