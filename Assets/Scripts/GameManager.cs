95ae1ab8a6c744ff190b29cd7b03e553151e29fe
Isabel <isadede@alum.us.es>
2022-01-05 19:28:41 +0100

    menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,20 +5,62 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    
+    private GameObject Simon;
+    public bool startingLevel;
+    private Vector2 obj;
+    private string nameLevel;
+
+    private void Start()
+    {
+        Simon = GameObject.Find("Ghost");
+        startingLevel = false;
+    }
+
     void Update()
     {
         if (GameObject.FindGameObjectsWithTag("NPC").Length == 0 && GameObject.FindGameObjectsWithTag("Exorcist").Length == 0 &&
             SceneManager.GetActiveScene().name == "Level 0")
         {
-            Debug.Log("You Win");
             SceneManager.LoadScene("Win");
+        }
+        if (startingLevel)
+        {
+            Simon.transform.position = Vector2.MoveTowards(Simon.transform.position, obj, 1.5f * Time.deltaTime);
+            if (Vector2.Distance(Simon.transform.position, new Vector2(-4.7f, 0)) < 0.01f)
+            {
+                startingLevel = false;
+                SceneManager.LoadScene(nameLevel);
+            }
         }
     }
 
+    public void StartLevel1()
+    {
+        GameObject.Find("Story 6").gameObject.SetActive(false);
+        obj = new Vector2(-4.7f, 0);
+        nameLevel = "Level 0";
+        startingLevel = true;
+    }
+
+    public void StartLevel2()
+    {
+        GameObject.Find("Story 6").gameObject.SetActive(false);
+        new Vector2(-4.7f, 0);
+        nameLevel = "Level 0";
+        startingLevel = true;
+    }
+
+    public void StartLevel3()
+    {
+        GameObject.Find("Story 6").gameObject.SetActive(false);
+        new Vector2(-4.7f, 0);
+        nameLevel = "Level 0";
+        startingLevel = true;
+    }
 
     public void StartGame()
     {
+
         SceneManager.LoadScene("Level 0");
         /*
         GameObject.Find("Player").GetComponent<Player>().hp = 200;
